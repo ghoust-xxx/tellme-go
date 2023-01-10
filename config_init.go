c0e7950145a38d91a49471769129887c1949d5bb
Alex Ghoust <ghoust@pinacotheca.net>
2023-01-10 11:52:59 +0600

    Create default config file if it is does not exist yet

--- config_init.go
+++ config_init.go
@@ -8,10 +8,17 @@ import (
 	"path/filepath"
 )
 
+const confDirName = "tellme"
+const confFileName = "config"
+const cacheDirName = "cache"
+
 // configInit makes sure config file and cache dir exist and read config values.
 func configInit() {
 	checkConfig()
 	checkCache()
+	fmt.Println(confDir)
+	fmt.Println(confFile)
+	fmt.Println(cacheDir)
 }
 
 // checkConfig checks if $HOME/.tellme/ || $XDG_CONFIG_HOME/tellme/,
@@ -48,8 +55,57 @@ func checkConfig() {
 	confFile = filepath.Join(confDir, confFileName)
 	_, err = os.Stat(confFile)
 	if errors.Is(err, os.ErrNotExist) {
-		// TODO: create a new conf file
-		fmt.Println("create a new conf file")
+		createNewConf()
+	}
+}
+
+// createNewConf write a new config file with all default values and comments
+func createNewConf() {
+	fmt.Println("create a new conf file")
+
+	f, err := os.OpenFile(confFile, os.O_RDWR|os.O_CREATE, 0750)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		err = f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	defaultConfig :=
+		`# TellMe configuration file
+
+# interactive mode (yes|no)
+INTERACTIVE=no
+
+# check existence of pronunciation (yes|no)
+PRONUNCIATION_CHECK=yes
+
+# download audiofiles in current directory (yes|no)
+DOWNLOAD=yes
+
+# cache files (yes|no)
+CACHE=yes
+
+# cache directory. Default is \$HOME/.tellme/cache
+CACHE_DIR=\$HOME/.tellme/cache
+
+# language (en, es, de, etc). Default is 'nl'
+LANG=nl
+
+# audiofiles type (mp3|ogg). mp3 by default
+TYPE=mp3
+
+# verbose mode (yes|no)
+VERBOSE=no
+
+`
+	_, err = f.WriteString(defaultConfig)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
--- tellme.go
+++ tellme.go
@@ -6,16 +6,8 @@ import (
 
 var confDir, confFile, cacheDir string
 
-const confDirName = "tellme"
-const confFileName = "config"
-const cacheDirName = "cache"
-
 func main() {
 	fmt.Println("Hello, world!\n")
 
 	configInit()
-	fmt.Println(confDir)
-	fmt.Println(confFile)
-	fmt.Println(cacheDir)
 }
-
